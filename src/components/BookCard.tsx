949dee318558273966227d693e4e7a7d8af30ecb
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-03-29 15:53:38 +0300

    build

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,31 +1,45 @@
-import { MouseEventHandler } from "react";
-import { Book } from "./Main";
+import { MouseEventHandler } from 'react';
+import { Book } from './Main';
 
-const BookCard = ({show, item, onClose}: {show:Boolean, item:Book, onClose:MouseEventHandler}) => {
-    if(!show) {
-        return null;
-    }
-    let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-    return (
-        <>
-        <div className="overlay">
-            <div className="overlay-inner">
-                <button className="close" onClick={onClose}>X</button>
-                <div className="inform-box">
-                    <img src={thumbnail}/>
-                    <div className="info">
-                        <h1>{item.volumeInfo.title}</h1>
-                        <h3>{item.volumeInfo.authors}</h3>
-                        <h4>{item.volumeInfo.publisher} <span>{item.volumeInfo.publishedDate}</span></h4>
-                        <a href={item.volumeInfo.previewLink} target="_blank"><button className="linkToPage">Go to book page</button></a>
-                        <br/>
-                    </div>
-                </div>
-                <h4 className="description">{item.volumeInfo.description}</h4>
+const BookCard = ({
+  show,
+  item,
+  onClose,
+}: {
+  show: Boolean;
+  item: Book;
+  onClose: MouseEventHandler;
+}) => {
+  if (!show) {
+    return null;
+  }
+  let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+  return (
+    <>
+      <div className="overlay">
+        <div className="overlay-inner">
+          <button className="close" onClick={onClose}>
+            X
+          </button>
+          <div className="inform-box">
+            <img src={thumbnail} />
+            <div className="info">
+              <h1>{item.volumeInfo.title}</h1>
+              <h3>{item.volumeInfo.authors}</h3>
+              <h4>
+                {item.volumeInfo.publisher} <span>{item.volumeInfo.publishedDate}</span>
+              </h4>
+              <a href={item.volumeInfo.previewLink} target="_blank">
+                <button className="linkToPage">Go to book page</button>
+              </a>
+              <br />
             </div>
+          </div>
+          <h4 className="description">{item.volumeInfo.description}</h4>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default BookCard
+export default BookCard;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,37 +1,39 @@
-import { useState } from "react";
-import BookCard from "./BookCard";
-import { Book } from "./Main";
-import React from "react";
+import { useState } from 'react';
+import BookCard from './BookCard';
+import { Book } from './Main';
+import React from 'react';
 
 const Card = ({ book }: { book: Book[] }) => {
-
-  const [show, setShow] = useState<boolean>(false)
-  const [bookItem, setBookItem] = useState<Book>(Object)
+  const [show, setShow] = useState<boolean>(false);
+  const [bookItem, setBookItem] = useState<Book>(Object);
 
   return (
-    
     <>
       {book.map((item: Book) => {
-
         let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
         let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
         let author = item.volumeInfo && item.volumeInfo.authors;
 
-        if(thumbnail!= undefined && amount !=undefined) {
-        
-        return (
-        <React.Fragment key={item.id}>
-          <div className="card" onClick={()=>{setShow(true); setBookItem(item)}}>
-            <img src={thumbnail} />
-            <div className="inform">
-              <h3 className="title">{item.volumeInfo.title}</h3>
-              <h4 className="author">{author}</h4>
-              <h4 className="amount">{amount} &#8381;</h4>
-            </div>
-          </div>
-          <BookCard show={show} item={bookItem} onClose={()=>setShow(false)} />
-          </React.Fragment>
-        );
+        if (thumbnail != undefined && amount != undefined) {
+          return (
+            <React.Fragment key={item.id}>
+              <div
+                className="card"
+                onClick={() => {
+                  setShow(true);
+                  setBookItem(item);
+                }}
+              >
+                <img src={thumbnail} />
+                <div className="inform">
+                  <h3 className="title">{item.volumeInfo.title}</h3>
+                  <h4 className="author">{author}</h4>
+                  <h4 className="amount">{amount} &#8381;</h4>
+                </div>
+              </div>
+              <BookCard show={show} item={bookItem} onClose={() => setShow(false)} />
+            </React.Fragment>
+          );
         }
       })}
     </>
