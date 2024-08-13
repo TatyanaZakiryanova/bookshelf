99f383e3a45e3653b02c041b77fef8d9fbe8508d
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-08-13 22:21:49 +0300

    search component fixed

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -5,28 +5,37 @@ import { useAppDispatch } from '../../redux/store';
 import styles from './Pagination.module.scss';
 import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi';
 import { ImFirst } from 'react-icons/im';
+import { useEffect } from 'react';
 
 const Pagination = () => {
   const dispatch = useAppDispatch();
   const startIndex = useSelector(startIndexSelector);
 
+  const currentPage = Math.floor(startIndex / 40) + 1;
+
   const onClickPage = (startIndex: number) => {
     dispatch(setStartIndex(startIndex));
-    window.scrollTo({ top: 100, behavior: 'smooth' });
   };
 
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [startIndex]);
+
   return (
-    <div className={styles.pagination}>
-      <button onClick={() => onClickPage(0)} disabled={startIndex === 0}>
-        <ImFirst size={20} className={styles.arrow} />
-      </button>
-      <button onClick={() => onClickPage(startIndex - 40)} disabled={startIndex === 0}>
-        <BiSolidLeftArrow size={15} className={styles.arrow} />
-      </button>
-      <button onClick={() => onClickPage(startIndex + 40)}>
-        <BiSolidRightArrow size={15} className={styles.arrow} />
-      </button>
-    </div>
+    <>
+      <div className={styles.pagination}>
+        <button onClick={() => onClickPage(0)} disabled={startIndex === 0}>
+          <ImFirst size={20} className={styles.arrow} />
+        </button>
+        <button onClick={() => onClickPage(startIndex - 40)} disabled={startIndex === 0}>
+          <BiSolidLeftArrow size={15} className={styles.arrow} />
+        </button>
+        <button onClick={() => onClickPage(startIndex + 40)}>
+          <BiSolidRightArrow size={15} className={styles.arrow} />
+        </button>
+      </div>
+      <div className={styles.page}>{currentPage}</div>
+    </>
   );
 };
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useRef, KeyboardEvent } from 'react';
+import { ChangeEvent, useRef, KeyboardEvent, useState } from 'react';
 import { useAppDispatch } from '../../redux/store';
-import { setSearchValue } from '../../redux/searchSlice/searchSlice';
+import { setSearchValue, setStartIndex } from '../../redux/searchSlice/searchSlice';
 import { fetchBooks } from '../../redux/booksSlice/asyncActions';
 import { FaDeleteLeft } from 'react-icons/fa6';
 import { GrSearch } from 'react-icons/gr';
@@ -11,37 +11,42 @@ import {
   langRestrictValueSelector,
   orderByParameterSelector,
   searchSelector,
-  startIndexSelector,
 } from '../../redux/searchSlice/selectors';
 
 const Search = () => {
   const dispatch = useAppDispatch();
 
+  const [localSearch, setLocalSearch] = useState('');
+
   const search = useSelector(searchSelector);
-  const startIndex = useSelector(startIndexSelector);
   const filter = useSelector(filterValueSelector);
   const langRestrict = useSelector(langRestrictValueSelector);
   const orderBy = useSelector(orderByParameterSelector);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const setValue = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchValue(e.target.value));
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocalSearch(e.target.value);
+  };
+
+  const handleSearchSubmit = async () => {
+    dispatch(setStartIndex(0));
+    dispatch(setSearchValue(localSearch));
+    await dispatch(
+      fetchBooks({ search: localSearch, orderBy, filter, startIndex: 0, langRestrict }),
+    );
+    inputRef.current?.blur();
   };
 
   const clearInput = () => {
+    setLocalSearch('');
     dispatch(setSearchValue(''));
     inputRef.current?.focus();
   };
 
-  const getBooks = async () => {
-    await dispatch(fetchBooks({ search, orderBy, filter, startIndex, langRestrict }));
-  };
-
   const searchKey = (e: KeyboardEvent) => {
     if (e.key === 'Enter') {
-      getBooks();
-      inputRef.current?.blur();
+      handleSearchSubmit();
     }
   };
 
@@ -53,8 +58,8 @@ const Search = () => {
             ref={inputRef}
             type="text"
             placeholder="Enter book name..."
-            value={search}
-            onChange={setValue}
+            value={localSearch}
+            onChange={handleInputChange}
             onKeyUp={searchKey}
           />
           {search && (
@@ -62,7 +67,7 @@ const Search = () => {
               <FaDeleteLeft className={styles.clearicon} onClick={clearInput} />
             </span>
           )}
-          <button onClick={getBooks}>
+          <button onClick={handleSearchSubmit}>
             <GrSearch className={styles.searchbutton} size={20} /> Search
           </button>
         </div>
