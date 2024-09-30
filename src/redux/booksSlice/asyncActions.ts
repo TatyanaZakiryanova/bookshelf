b50640242d5a0aaf96b7770e240a82a093cc2134
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-09-30 15:56:08 +0300

    api key hidden

--- src/redux/booksSlice/asyncActions.ts
+++ src/redux/booksSlice/asyncActions.ts
@@ -7,9 +7,11 @@ export const fetchBooks = createAsyncThunk<Book[], SearchParams>(
   'books/fetchBooks',
   async (params, { rejectWithValue }) => {
     const { search, orderBy, filter, startIndex, langRestrict } = params;
+    const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
+
     try {
       const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyCeC6XVMuZOAY3TjODjgT7R5Joc4qHcjEE&orderBy=${orderBy}&filter=${filter}&startIndex=${startIndex}&maxResults=40&langRestrict=${langRestrict}&projection=lite`,
+        `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${apiKey}&orderBy=${orderBy}&filter=${filter}&startIndex=${startIndex}&maxResults=40&langRestrict=${langRestrict}&projection=lite`,
       );
 
       const books: Book[] = response.data.items;
